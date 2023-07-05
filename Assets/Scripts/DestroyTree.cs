ecb06143a8484995d54854d3613344646070c636
Emin Duran <1eminduran@gmail.com>
2023-07-05 22:45:38 +0300

    Mining
    Tree , Stone , Car prefablarına vurma ve yok etme animasyonları ve kodları yazıldı .

---
+++ Assets/Scripts/DestroyTree.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class DestroyTree : MonoBehaviour
+{
+    public GameObject[] agacBolumleri;
+    public float waitTime = 1.5f;
+    private int currentIndex = 0;
+    private float timer = 0f;
+
+   private void OnTriggerEnter(Collider other)
+{
+    if (other.gameObject.CompareTag("Player")) 
+    {
+        Animator otherAnimator = other.gameObject.GetComponent<Animator>();
+        Debug.Log("isNearTree değeri: " + otherAnimator.GetBool("isNearTree"));
+        Debug.Log("Merhaba");
+        timer = 0f;
+        
+
+    }
+}
+
+private void OnTriggerStay(Collider other)
+{
+    
+    if (other.gameObject.CompareTag("Player")) 
+    {
+        Animator otherAnimator = other.gameObject.GetComponent<Animator>();
+        bool isNearTree = otherAnimator.GetBool("isNearTree");
+        Debug.Log("isNearTree değeri: " + otherAnimator.GetBool("isNearTree"));
+        Debug.Log("Merhaba   : " +timer);
+        if(isNearTree){
+            timer += Time.deltaTime;
+            if(timer>=waitTime && currentIndex < agacBolumleri.Length ){
+                Destroy(agacBolumleri[currentIndex]);
+                currentIndex++;
+                timer = 0f;
+                            
+            }
+        }
+
+    }
+}
+    
+}
--- Assets/Scripts/StoneAnimation.cs
+++ Assets/Scripts/StoneAnimation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreeAnimationControl : MonoBehaviour
+public class StoneAnimation : MonoBehaviour
 {
     public float distanceThreshold = 2f; // Ağaca olan minimum mesafe
     public float waitTime = 3f; // Bekleme süresi
@@ -10,6 +10,7 @@ public class TreeAnimationControl : MonoBehaviour
     private Touch _touch;
     private float timer = 0f; // Zamanlayıcı
     public bool isNearCharacter = false;
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -19,41 +20,40 @@ public class TreeAnimationControl : MonoBehaviour
     void Update()
     {
         // Tagı "tree" olan tüm nesneleri al
-        GameObject[] trees = GameObject.FindGameObjectsWithTag("tree");
+        GameObject[] stones = GameObject.FindGameObjectsWithTag("stone");
 
-        foreach (GameObject tree in trees)
+        foreach (GameObject stone in stones)
         {
             // Karakterin ve ağacın konumları arasındaki mesafeyi hesapla
-            float distanceToTree = Vector3.Distance(transform.position, tree.transform.position);
+            float distanceToStone = Vector3.Distance(transform.position, stone.transform.position);
              if(Input.touchCount >0){
             _touch =Input.GetTouch(0);
             
             }
             // Eğer karakter ağaca yakınsa ve daha önce ağaca yakın değilse
-            if (distanceToTree <= distanceThreshold  && !isNearCharacter)
+            if (distanceToStone <= distanceThreshold  && !isNearCharacter)
             {
                 // Bekleme süresini başlat
                 isNearCharacter = true;
                 timer = 0f;
             }
-            if (distanceToTree <= distanceThreshold && _touch.phase == TouchPhase.Ended)
+            if (distanceToStone <= distanceThreshold && _touch.phase == TouchPhase.Ended)
             {
                  if (isNearCharacter)
             {
                 // Zamanlayıcıyı artır
                 timer += Time.deltaTime;
-
                 // Bekleme süresini kontrol et
                 if (timer >= waitTime && timer <endWaitTime)
                 {
                     // Animasyonu başlat
-                    GetComponent<Animator>().SetBool("isNearTree", true);
+                    GetComponent<Animator>().SetBool("isNearStone", true);
                     
                 }
                 else if(timer >= endWaitTime)
                 {
                     // Bekleme süresi dolmadı, animasyonu durdur
-                    GetComponent<Animator>().SetBool("isNearTree", false);
+                    GetComponent<Animator>().SetBool("isNearStone", false);
                     timer = 0f;
                 }
             }
