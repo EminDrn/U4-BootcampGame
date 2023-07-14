6061df2b05bad73026b17fc49fae7c544d086509
BenAytu <aytugbengu2000@gmail.com>
2023-07-15 02:27:24 +0300

    Tree_Stone_Car_Par&Col_Bug_Fixed
    slm aşko

--- Assets/Scripts/DestroyTree.cs
+++ Assets/Scripts/DestroyTree.cs
@@ -7,6 +7,10 @@ public class DestroyTree : MonoBehaviour
     private int currentIndex = 0;
     private float timer = 0f;
 
+    [SerializeField] private ParticleSystem[] par_Tree;
+    [SerializeField] private GameObject woodCol;
+    [SerializeField] private Transform spawnPos;
+
    private void OnTriggerEnter(Collider other)
 {
     if (other.gameObject.CompareTag("Player")) 
@@ -33,6 +37,9 @@ private void OnTriggerStay(Collider other)
             timer += Time.deltaTime;
             if(timer>=waitTime && currentIndex < agacBolumleri.Length ){
                 Destroy(agacBolumleri[currentIndex]);
+                par_Tree[0].Play();
+                par_Tree[1].Play();
+                Instantiate(woodCol, spawnPos.position, Quaternion.identity);
                 currentIndex++;
                 timer = 0f;
                             
