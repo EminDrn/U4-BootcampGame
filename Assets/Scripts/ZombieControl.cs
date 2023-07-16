655b35baaa4bb8704d3620883f4515a17f576920
BenAytu <aytugbengu2000@gmail.com>
2023-07-17 00:49:07 +0300

    Zombie_Death_Added_w/Beyza
    Zombi öldürme eklendi

--- Assets/Scripts/ZombieControl.cs
+++ Assets/Scripts/ZombieControl.cs
@@ -1,68 +1,121 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class ZombieControl : MonoBehaviour
 {
-//    // Start is called before the first frame update
-    [SerializeField] Animator Anim;
-    [SerializeField]
-    public static float HP =100;
-    private float maxHp =100;
-    bool isAlive = true;
-    [SerializeField] FloatingHealthBar healthBar;
-    public GameObject Zombie;
-    
-    [SerializeField] Canvas sliderZombie;
-    [SerializeField] Transform cameraTarget;
+    [SerializeField] private Rigidbody rbZombie;
+    [SerializeField] private GameObject followTarget;
+    [SerializeField] private Animator chAnim;
+    private NavMeshAgent _agent;
+    [SerializeField] private float distance;
+    [SerializeField] private float followDistance;
+    [SerializeField] private float attackDistance;
 
-    // private void Awake() {
-    //     healthBar = GetComponentInChildren<FloatingHealthBar>();
-    // }
+    public GameObject cameraObj;
+    public Transform cameraTarget;
+
+    [SerializeField] private Slider zombieSlider;
+    [SerializeField] private Canvas followSlider;
+
+    [SerializeField] private float zombieHealth;
+    [SerializeField] private float zombieMaxHealth;
+    [SerializeField] private Animator zombiAnim;
+    [SerializeField] private bool _zombieDead = false;
+
+    // Start is called before the first frame update
     void Start()
     {
-        Anim = GetComponent<Animator>();
-        Zombie = GameObject.Find("Zombie Transition (1)");
-    
-        //Anim = Zombie.GetComponent<Animator>();
-        healthBar.UpdateHealthBar(HP, maxHp);
-
+        followTarget = GameObject.Find("AliCabbar");
+        chAnim = followTarget.GetComponent<Animator>();
+        cameraObj = GameObject.Find("Main Camera");
+        cameraTarget = cameraObj.GetComponent<Transform>();
+        _agent = GetComponent<NavMeshAgent>();
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        sliderZombie.transform.LookAt(cameraTarget);
-        //GameObject[] zombies = GameObject.FindGameObjectsWithTag("zombie");
-        //foreach(var zombi in zombies ){
-        //Anim = zombi.GetComponent<Animator>();
-        //Debug.Log(zombi.name + "  " + HP+ " ---");
-        if(HP<=0){
-            isAlive = false;
-            Anim.SetBool("isAlive", false );
-        }else{
-            Anim.SetBool("isAlive", true );
+        followSlider.transform.LookAt(cameraTarget);
 
+        if (_agent.enabled == false)
+        {
+            return;
         }
-            if(isAlive == false)
+        if (!_zombieDead)
+        {
+            distance = Vector3.Distance(this.transform.position, followTarget.transform.position);
+            if (distance < followDistance)
             {
-                Anim.Play("death");
+                _agent.transform.LookAt(followTarget.transform);
+                _agent.isStopped = false;
+                _agent.SetDestination(followTarget.transform.position);
+                zombiAnim.SetBool("isMoving", true);
+                zombiAnim.SetBool("isAttack", false);
+
             }
-        //}
-        
+            else
+            {
+                _agent.isStopped = true;
+                zombiAnim.SetBool("isMoving", false);
+                zombiAnim.SetBool("isAttack", false);
+            }
+            if (distance < attackDistance)
+            {
+                _agent.isStopped = true;
+                zombiAnim.SetBool("isMoving", false);
+                zombiAnim.SetBool("isAttack", true);
+            }
+        }
+        if (zombieHealth <= 0)
+        {
+            _zombieDead = true;
+            zombiAnim.SetBool("isAlive", false);
+        }
+        if (_zombieDead)
+        {
+            _agent.isStopped = true;
+            zombiAnim.Play("Zombie_Dying");
+            chAnim.SetBool("isNearZombie", false);
+            this.GetComponent<Rigidbody>().useGravity = false;
+            this.GetComponent<Rigidbody>().mass = 0.01f;
+            this.GetComponent<Collider>().enabled = false;
+            followTarget.GetComponent<Collider>().enabled = false;
+
+            StartCoroutine(Disappear());
+        }
+    }
+
+    private void UpdateZombieHealthBar()
+    {
+        zombieSlider.value = zombieHealth / zombieMaxHealth;
+    }
+
+    private IEnumerator Disappear()
+    {
+        followTarget.GetComponent<Collider>().enabled = true;
+        yield return new WaitForSeconds(4f);
+        _agent.enabled = false; // farklý yöntem bulmamýz lazým. isStopped hata alýyoruz.
+        Vector3 a = transform.position;
+        Vector3 b = this.transform.position;
+        float t = 0.01f;
+        b.y = -0.3f;
+        this.gameObject.transform.position = Vector3.Lerp(a, b, t);
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
     }
     public void ZombiyeHasarVer(){       
-        HP -= Random.Range(20,25);
-        healthBar.UpdateHealthBar(HP, maxHp);
-        Debug.Log("22222222");
-        HpDondur();
+        zombieHealth -= Random.Range(20,25);
+        UpdateZombieHealthBar();
+        rbZombie.AddForce(-transform.forward * 100000);
+    }
 
+    public void ZombieGiveDamage()
+    {
+        followTarget.GetComponent<CharacterControl>().HasarAl();
+        Debug.Log("Can Azaldý");
     }
-    public float HpDondur(){
-        float can = HP;
-        return HP;
-    }
-   
+
 }
